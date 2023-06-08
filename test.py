929e45b4e707893b0f6e540fb0cb7b8acda77a99
gatienc <gatien.chenu@gmail.com>
2023-06-08 19:53:57 +0200

    Cartobat data discovery

---
+++ test.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+data= pd.read_csv("data/data.csv")
+
+data["timestamp"]=pd.to_datetime(data['timestamp'])
+print(data.head(5))
+#data=data[(data['timestamp'] > '2023-06-01 09:50:00.000000')&(data['timestamp'] < '2023-06-01 09:55:30.000000')]
+
+
+test=sns.lineplot(  
+    data=data, hue="macModule",
+    x="timestamp", y="rssi", 
+)
+ 
+plt.xlabel('timestamp')
+plt.xticks(rotation=100)
+plt.ylabel('rssi')
+plt.title('RSSI values for tag ')
+plt.legend(loc='upper right', fontsize='large')
+
+plt.show()
