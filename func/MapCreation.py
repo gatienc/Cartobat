b7ce3a6fb1e40c9409c497305e84c3c8554708ab
gchenu <gatien+tsp@chenu.me>
2023-06-26 15:37:09 +0200

    Folium zone layer builder

--- func/MapCreation.py
+++ func/MapCreation.py
@@ -15,10 +15,24 @@ def MapCreation(map_gdf,marker_gdf):
     folium.TileLayer('stamenterrain').add_to(m)
     tooltip = folium.features.GeoJsonTooltip(fields=['ID_element', 'nom'], 
                                             labels=True,
-                                            stick=False)
+                                            stick=False,
+                                            )
     tooltip_marker = folium.features.GeoJsonTooltip(fields=['macModule'], 
                                             labels=True,
                                             stick=False)
     folium.GeoJson(map_gdf, tooltip=tooltip).add_to(m)
     folium.GeoJson(marker_gdf, tooltip=tooltip_marker).add_to(m)
     return m
+
+def FoliumZoneLayer(map_gdf:gpd.GeoDataFrame,color='blue'):
+    ZoneLayer=folium.GeoJson(map_gdf, 
+                    tooltip = folium.features.GeoJsonTooltip(fields=['ID_element', 'nom'], 
+                                            labels=True,
+                                            stick=False,
+                                            ),
+                    style_function=lambda x: {
+                                'fillColor': color,
+                                'fillOpacity': 0.5,
+                                'weight': 0.1
+                            })
+    return ZoneLayer
