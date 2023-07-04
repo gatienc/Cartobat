cde0cc9aae6d8aed019895b91eeaf7a330fdbb59
gchenu <gatien+tsp@chenu.me>
2023-07-04 12:12:16 +0200

    better type checking

--- src/preprocessing/Preprocessor.py
+++ src/preprocessing/Preprocessor.py
@@ -1,10 +1,11 @@
 
 
 class Preprocessor:
-    def __init__(self,rssi_df) -> None:
+    def __init__(self,rssi_df,sample_time) -> None:
         self.rssi_df=rssi_df
         self.filter=None
         self.cleaner=None
+        self.sample_time=sample_time
     def set_cleaner(self,cleaner:function)->'Preprocessor':
         self.cleaner=cleaner
         return self
@@ -14,13 +15,15 @@ class Preprocessor:
     def format_rssi_df(self)->'Preprocessor':
         raw_rssi_df=self.rssi_df.json()
         return self
-        
     def process(self)->'Preprocessor':
-        if self.cleaner==None:
-            raise Exception("Cleaner is not set, please use set_cleaner(cleaner:function)")
+        if not isinstance(self.cleaner, function):
+            raise TypeError("Cleaner is not set, please use set_cleaner(cleaner:function)")
  
-        if self.filter==None:
-            raise Exception("Filter is not set, please use set_filter(filter:function)")
+        if not isinstance(self.cleaner, function):
+            raise TypeError("Filter is not set, please use set_filter(filter:function)")
+        mac_module_list=self.rssi_df['macModule'].unique()
+        for mac_module in mac_module_list:
+            self.filter(self.rssi_df[self.rssi_df['mac_module']==mac_module]) # type: ignore
         self.rssi_df=self.cleaner(self.rssi_df) # type: ignore
         self.rssi_df=self.filter(self.rssi_df) # type: ignore
         return self.rssi_df
