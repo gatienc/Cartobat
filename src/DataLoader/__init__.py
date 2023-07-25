e72af1bb6d058d930e1dbfbba62d4158a7a710e4
gchenu <gatien+tsp@chenu.me>
2023-07-25 14:59:58 +0200

    room receiver in interface

--- src/DataLoader/__init__.py
+++ src/DataLoader/__init__.py
@@ -1,3 +1,5 @@
 from .. _DataLoader.DataLoader import gdfLoader,MarkerLoader,RSSIDataLoader
 from .. _DataLoader.save.map import save_map
 from .. _DataLoader.save.marker import save_cartomodule
+from .. _DataLoader.Room import Room
+from .. _DataLoader.Receiver import Receiver
